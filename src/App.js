980d016166ba73139df698e27e42cc391c301f9f
Luan Victor <76531786+Luzin7@users.noreply.github.com>
2022-06-17 15:34:33 -0300

    feat: add page routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react'
-import Login from './components/Login'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Home from './pages/home/Home'
+import Ajuda from './pages/Ajuda';
+import Cadastro from './pages/Cadastro';
+import NotFound from './pages/NotFound';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        <Login />
-      </div>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Login/>}/>
+          <Route path='/ajuda' element={<Ajuda/>}/>
+          <Route path='/cadastro' element={<Cadastro/>}/>
+          <Route path='/home' element={<Home/>}/>
+          <Route path='*' element={<NotFound />}/>
+        </Routes>
+      </Router>
     )
   }
 }

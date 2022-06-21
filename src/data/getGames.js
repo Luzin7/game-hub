ec99702bd3ebf088a63ba0bb0aed631bf566ebe9
Luan Victor <76531786+Luzin7@users.noreply.github.com>
2022-06-20 21:31:41 -0300

    feat: fake API to games

---
+++ src/data/getGames.js
@@ -0,0 +1,63 @@
+export const GAMES = [
+  {
+    id: "League-of-Legends",
+    name: "League of Legends",
+    img: "https://files.tecnoblog.net/wp-content/uploads/2019/05/league-of-legends-700x394.jpg",
+    description:
+      "League of Legends é um jogo de estratégia em que duas equipes de cinco poderosos Campeões se enfrentam para destruir a base uma da outra. Escolha entre mais de 140 Campeões para realizar jogadas épicas, assegurar abates e destruir torres conforme você luta até a vitória.",
+    media:
+      "https://c.tenor.com/EAjohp6VzvgAAAAC/league-of-legends-riot-games.gif",
+    url: "https://www.leagueoflegends.com",
+  },
+  {
+    id: "valorant",
+    name: "Valorant",
+    img: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a72e03e1-b041-4795-a5ff-c506c722b8d5/demrj30-2900a20c-ab1e-4944-a507-ca0ce3ac9894.jpg/v1/fill/w_1024,h_577,q_75,strp/valorant_fanart_by_dankendi_demrj30-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NTc3IiwicGF0aCI6IlwvZlwvYTcyZTAzZTEtYjA0MS00Nzk1LWE1ZmYtYzUwNmM3MjJiOGQ1XC9kZW1yajMwLTI5MDBhMjBjLWFiMWUtNDk0NC1hNTA3LWNhMGNlM2FjOTg5NC5qcGciLCJ3aWR0aCI6Ijw9MTAyNCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.JZxmJVb2_RbDzVASelPNfJ8DTI6FuVebTFVNmHHyOaQ",
+    description: "Um jogo de tiro tático 5x5 com personagens originais.",
+    description__long:
+      "Misture o seu estilo e experiência em um cenário global e competitivo. Você terá 13 rodadas para atacar e defender com disparos certeiros e habilidades táticas. Com apenas uma vida por rodada, você deve pensar mais rápido que o oponente se quiser sobreviver. Encare inimigos nos modos Competitivo e Sem Ranque, além da Disputa da Spike e do Mata-Mata!",
+    media: "https://c.tenor.com/rkPKFm0AtvsAAAAC/sova-valorant.gif",
+    url: "https://playvalorant.com",
+  },
+  {
+    id: "warframe",
+    name: "Warframe",
+    img: "https://www.oficinadanet.com.br/imagens/post/35877/warframe-capa.jpg",
+    description:
+      "Warframe é um jogo online multiplayer de tiro em terceira pessoa gratuito para jogar desenvolvido e publicado pela Digital Extremes.",
+    description__long:
+      "Warframe coloca o jogador no papel de ninjas Tenno, em uma guerra com “alienígenas” que são clones humanos militarizados, os Grineer. Se não bastasse, ainda é preciso confrotar um outro grupo corporativo, a Corpus, e os Infectados, vítimas de um vírus mortal.",
+    media: "https://c.tenor.com/-Z-o8TS3LScAAAAC/bullets-dodging.gif",
+    url: "https://www.warframe.com/",
+  },
+  {
+    id: "brawlhalla",
+    name: "Brawlhalla",
+    img: "https://staticctf.akamaized.net/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/3fRtFxUJ37SKFvDWXF8mqI/4c08c54fc0136d9033b3680d7b9de7fc/brawlhalla-ubicom-page-meta-logo-960x540.png",
+    description:
+      "Mussum Ipsum, cacilds vidis litro abertis. Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis.Suco de cevadiss deixa as pessoas mais interessantis.Manduma pindureta quium dia nois paga.Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.",
+    description__long:
+      "Mussum Ipsum, cacilds vidis litro abertis. Cevadis im ampola pa arma uma pindureta.Vehicula non. Ut sed ex eros. Vivamus sit amet nibh non tellus tristique interdum.Posuere libero varius.",
+    url: "https://www.brawlhalla.com/",
+  },
+  {
+    id: "forza-horizon-5",
+    name: "Forza Horizon 5",
+    img: "https://e-esportes.com.br/wp-content/uploads/2021/10/forza-gamescom-hero-1.jpg",
+    description:
+      "Mussum Ipsum, cacilds vidis litro abertis. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.Aenean aliquam molestie leo, vitae iaculis nisl.Quem manda na minha terra sou euzis!Suco de cevadiss deixa as pessoas mais interessantis.",
+    description__long:
+      "Mussum Ipsum, cacilds vidis litro abertis. Cevadis im ampola pa arma uma pindureta.Vehicula non. Ut sed ex eros. Vivamus sit amet nibh non tellus tristique interdum.Posuere libero varius.",
+    url: "https://www.xbox.com/pt-br/games/store/forza-horizon-5-edicao-padrao/9nkx70bbcdrn",
+  },
+  {
+    id: "rocket-league",
+    name: "Rocket League",
+    img: "https://www.gamerpoint.com.br/wp-content/uploads/2020/09/rocket-league-free-to-play-.jpg",
+    description:
+      "Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mim que vai caçá sua turmis!Leite de capivaris, leite de mula manquis sem cabeça.Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis.Cevadis im ampola pa arma uma pindureta.",
+    description__long:
+      "Mussum Ipsum, cacilds vidis litro abertis. Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis!Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis.Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi.Aenean aliquam molestie leo, vitae iaculis nisl.",
+    url: "https://www.rocketleague.com/",
+  },
+];

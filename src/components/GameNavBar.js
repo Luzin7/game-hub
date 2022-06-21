74401dc1348ba2537abff06ddf64c86e3d09bd42
Luan Victor <76531786+Luzin7@users.noreply.github.com>
2022-06-20 21:29:53 -0300

    feat: add NavBar for game pages

---
+++ src/components/GameNavBar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+
+import { GAMES } from "../data/getGames";
+
+import "../style/components/gameNavBar.css";
+
+export default function HomeNavBar() {
+
+  const params = useParams();
+  const game = GAMES.find((game) => game.id === params.id)
+
+  return (
+    <div className="navbar game">
+      <h1>
+        <Link to={-1}>Voltar</Link>
+      </h1>
+      <p>{game.name}</p>
+    </div>
+  );
+}

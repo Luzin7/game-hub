076087215c6a79569a47b79e76ca49c77607acd4
Luan Victor <76531786+Luzin7@users.noreply.github.com>
2022-06-22 20:32:49 -0300

    feat: Get agent valorant

---
+++ src/data/getAgents.js
@@ -0,0 +1,97 @@
+export const AGENTS = [
+  {
+    id: "fade",
+    name: "Fade",
+    class: "Iniciador",
+  },
+  {
+    id: "breach",
+    name: "Breach",
+    class: "Iniciador",
+  },
+  {
+    id: "raze",
+    name: "Raze",
+    class: "duelista",
+  },
+  {
+    id: "chamber",
+    name: "Chamber",
+    class: "sentinela",
+  },
+  {
+    id: "kayo",
+    name: "KAY/O",
+    class: "Iniciador",
+  },
+  {
+    id: "skye",
+    name: "Skye",
+    class: "Iniciador",
+  },
+  {
+    id: "cypher",
+    name: "Cypher",
+    class: "sentinela",
+  },
+  {
+    id: "sova",
+    name: "Sova",
+    class: "Iniciador",
+  },
+  {
+    id: "killjoy",
+    name: "Killjoy",
+    class: "sentinela",
+  },
+  {
+    id: "viper",
+    name: "Viper",
+    class: "controlador",
+  },
+  {
+    id: "phoenix",
+    name: "Phoenix",
+    class: "duelista",
+  },
+  {
+    id: "astra",
+    name: "Astra",
+    class: "controlador",
+  },
+  {
+    id: "brimstone",
+    name: "Brimstone",
+    class: "controlador",
+  },
+  {
+    id: "neon",
+    name: "Neon",
+    class: "duelista",
+  },
+  {
+    id: "yoru",
+    name: "Yoru",
+    class: "duelista",
+  },
+  {
+    id: "sage",
+    name: "Sage",
+    class: "sentinela",
+  },
+  {
+    id: "reyna",
+    name: "Reyna",
+    class: "duelista",
+  },
+  {
+    id: "omen",
+    name: "Omen",
+    class: "controlador",
+  },
+  {
+    id: "jett",
+    name: "Jett",
+    class: "duelista",
+  },
+];

f2ff50eed1d17082981038d6d141717abfc9d4ab
Luan Victor <76531786+Luzin7@users.noreply.github.com>
2022-06-27 16:22:27 -0300

    refactor: change link path name

--- src/pages/Help.jsx
+++ src/pages/Help.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import * as Path from "../utils/pathNames";
 import NavHelp from "../components/NavHelp";
 
 import "../style/ajuda.css";
@@ -13,11 +14,14 @@ export default class Help extends Component {
           <h1>Não consigo entrar na minha conta. O que faço?</h1>
           <p>
             Caso você não esteja conseguindo acessar sua conta, talvez seja
-            porque você ainda não tenha uma. <Link to="/cadastro">Faça uma conta agora mesmo</Link> e tenha acesso ao Hub.
+            porque você ainda não tenha uma.{" "}
+            <Link to={Path.REGISTER}>Faça uma conta agora mesmo</Link> e tenha
+            acesso ao Hub.
           </p>
           <p>
-            Se tem uma conta mas não está conseguindo lembrar suas
-            informações de login, basta <Link to="/recuperar-login">recuperar o login aqui</Link>.
+            Se tem uma conta mas não está conseguindo lembrar suas informações
+            de login, basta{" "}
+            <Link to={Path.RECOVER_LOGIN}>recuperar o login aqui</Link>.
           </p>
         </div>
       </div>

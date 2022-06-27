00373e6eed38e52a77acc8fe475aa1c0fc83e6e6
Luan Victor <76531786+Luzin7@users.noreply.github.com>
2022-06-27 17:36:09 -0300

    refactor: Remove ternary operator

--- src/components/GameNavBar.js
+++ src/components/GameNavBar.js
@@ -2,33 +2,21 @@ import React from "react";
 import { Link, useParams } from "react-router-dom";
 
 import { GAMES } from "../data/getGames";
-import { AGENTS } from "../data/getAgents";
 
 import "../style/components/gameNavBar.css";
 
 export default function GameNavBar() {
   const params = useParams();
   const game = GAMES.find((game) => game.id === params.id);
-  const agent = AGENTS.find((game) => game.id === params.id);
 
   return (
     <div className="main__nav">
-      {params.id === "valorant" ? (
-        <div className="navbar game">
-          {" "}
-          <h1>
-            <Link to={-1}>Voltar</Link>
-          </h1>
-          <p>{game.name}</p>
-        </div>
-      ) : (
-        <div className="navbar game">
-          <h1>
-            <Link to={-1}>Voltar</Link>
-          </h1>
-          <p>{agent.name}</p>
-        </div>
-      )}
+      <div className="navbar game">
+        <h1>
+          <Link to={-1}>Voltar</Link>
+        </h1>
+        <p>{game.name}</p>
+      </div>
     </div>
   );
 }

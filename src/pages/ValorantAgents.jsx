476614ee892108276b0aaac8a1b70dd8c8deff6a
Luan Victor <76531786+Luzin7@users.noreply.github.com>
2022-06-27 16:36:32 -0300

    refactor: Delete comments

--- src/pages/ValorantAgents.jsx
+++ src/pages/ValorantAgents.jsx
@@ -16,11 +16,6 @@ export default class ValorantAgents extends React.Component {
             <div key={char.id} className="characters__box">
               <div className="character__box__content">
                 <Link to={`/jogos/valorant/agentes/${char.id}`}>
-                  {/* <a
-                  href={`https://playvalorant.com/pt-br/agents/${char.id}`}
-                  target="_blank"
-                  rel="noreferrer"
-                > */}
                   <img
                     src={`https://cdn.mobalytics.gg/assets/valorant/images/agents/full-size-small/${char.id}.png`}
                     alt={char.name}
@@ -31,7 +26,6 @@ export default class ValorantAgents extends React.Component {
                       <p>{char.class}</p>
                     </div>
                   </div>
-                  {/* </a> */}
                 </Link>
               </div>
             </div>

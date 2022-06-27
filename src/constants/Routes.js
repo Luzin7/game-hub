95d2766231f574a4f45a9235798156d1882a0977
Luan Victor <76531786+Luzin7@users.noreply.github.com>
2022-06-26 21:32:26 -0300

    docs: rename and move file

--- src/constants/Routes.js
+++
@@ -1,8 +0,0 @@
-export const LOGIN = "/";
-export const HELP = "/ajuda";
-export const REGISTER = "/cadastro";
-export const HOME = "/home";
-export const GAME = "/jogos/:id";
-
-// Valorant-LOL
-export const CHARACTERS = 'personagens'
--- src/constants/keyAPI.js
+++
@@ -1,1 +0,0 @@
-export const KEY = "RGAPI-e6c359db-1d2e-4e2a-8446-c18981caa143";

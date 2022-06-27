7deb1231e1afd435e335c77fe7e0fa8a1e7f452e
Luan Victor <76531786+Luzin7@users.noreply.github.com>
2022-06-27 08:15:44 -0300

    feat: Add agent page

--- src/pages/Agent.jsx
+++ src/pages/Agent.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router-dom";
 import { AGENTS } from ".././data/getAgents";
 
+import "../style/agent/agent.css";
+import "../style/agent/agentContent.css";
+import "../style/agent/agentInfo.css";
 
 function Agent() {
   const { id } = useParams();
   const agent = AGENTS.find((agent) => agent.id === id);
   return (
-    <div>
-      <h1>esse é o agente {agent.name}</h1>
-    </div>
-  )
+    <section className="agent">
+      <div className="agent__content">
+        <span>Algumas informações sobre {agent.name}</span>
+        <div className="agent__info">
+          <h1>Agente {agent.name}</h1>
+          <img
+            src={`https://cdn.mobalytics.gg/assets/valorant/images/agents/full-size-small/${agent.id}.png`}
+            alt={agent.name}
+          />
+          <h2>Função: {agent.class}</h2>
+          <div className="agent__description">
+            <p>{agent.description}</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Agent
+export default Agent;

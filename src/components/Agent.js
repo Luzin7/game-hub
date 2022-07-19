09dc360573baa0aea66e4efe25e9726d1f17cfe6
Luan Victor <76531786+Luzin7@users.noreply.github.com>
2022-07-19 20:44:32 -0300

    feat: Create Agent page's component

---
+++ src/components/Agent.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import { AGENTS } from "../data/getAgents";
+
+import "../style/agent/agent.css";
+import "../style/agent/agentContent.css";
+import "../style/agent/agentInfo.css";
+
+export function Agent() {
+  window.addEventListener("scroll", () => {
+    const img = document.querySelector("img[name=agent__img]");
+    let value = window.scrollY;
+    img.style.paddingTop = value * 1 + "px";
+  });
+
+  const { id } = useParams();
+  const agent = AGENTS.find((agent) => agent.id === id);
+  return (
+    <section className="agent">
+      <header className="navbar agent__navbar">
+        <h1>
+          <Link to={-1}>Voltar</Link>
+        </h1>
+        <p>{agent.name}</p>
+      </header>
+      <div className="agent__content">
+        <div className="agent__info">
+          <h1>Agente {agent.name}</h1>
+          <div className="agent__img">
+            <img
+              name="agent__img"
+              src={`https://cdn.mobalytics.gg/assets/valorant/images/agents/full-size-small/${agent.id}.png`}
+              alt={agent.name}
+            />
+          </div>
+          <div className="agent__description">
+            <h2>Função: {agent.class}</h2>
+            <p>{agent.description}</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
